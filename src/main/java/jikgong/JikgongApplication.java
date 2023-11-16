b6a9e47d1dbff80a1fcbe841a3d16ad249d484e5
dgjinsu <rlawlstn3206@naver.com>
2023-11-16 16:29:22 +0900

    first commit

---
+++ src/main/java/jikgong/JikgongApplication.java
@@ -0,0 +1,13 @@
+package jikgong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JikgongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JikgongApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jikgong/JikgongApplicationTests.java
@@ -0,0 +1,13 @@
+package jikgong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JikgongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
