019ff54b4c496550aab8f3d5179878866c78b8d4
dgjinsu <rlawlstn3206@naver.com>
2023-11-17 22:33:43 +0900

    feat: feign config 의존성 추가

--- src/main/java/jikgong/JikgongApplication.java
+++ src/main/java/jikgong/JikgongApplication.java
@@ -2,8 +2,10 @@ package jikgong;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class JikgongApplication {
 
 	public static void main(String[] args) {
