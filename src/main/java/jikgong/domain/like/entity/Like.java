f84351fa782472be55bd4701c82895a71853be5a
dgjinsu <rlawlstn3206@naver.com>
2023-12-01 17:51:26 +0900

    feat: 좋아요 관련 계층 생성

---
+++ src/main/java/jikgong/domain/like/entity/Like.java
@@ -0,0 +1,29 @@
+package jikgong.domain.like.entity;
+
+import jakarta.persistence.*;
+import jikgong.domain.common.BaseEntity;
+import jikgong.domain.member.entity.Member;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Table(name = "likes")
+public class Like extends BaseEntity {
+    /**
+     * 회사가 노동자에게 누르는 좋아요
+     */
+    @Id @GeneratedValue
+    @Column(name = "like_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "sender_id")
+    private Member sender; // 좋아요 누른 회원
+
+    @ManyToOne
+    @JoinColumn(name = "receiver_id")
+    private Member receiver; // 좋아요 받은 회원
+}
---
+++ src/main/java/jikgong/domain/like/repository/LikeRepository.java
@@ -0,0 +1,9 @@
+package jikgong.domain.like.repository;
+
+import jikgong.domain.like.entity.Like;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LikeRepository extends JpaRepository<Like, Long> {
+}
