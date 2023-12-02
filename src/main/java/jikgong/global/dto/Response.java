2af789ba7e546c2b3dcd20bb40dd75ce1a05ffc8
dgjinsu <rlawlstn3206@naver.com>
2023-12-02 21:20:37 +0900

    feat: json 데이터 반환 시 null인 필드 제외

--- src/main/java/jikgong/global/dto/Response.java
+++ src/main/java/jikgong/global/dto/Response.java
@@ -1,10 +1,13 @@
 package jikgong.global.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 
 @AllArgsConstructor
-@Data
+@Getter
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class Response<T> {
     private T data;
     private String message;
