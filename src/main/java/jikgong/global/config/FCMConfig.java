31954f66fa00762d77e32892858a3e8966a5431b
dgjinsu <rlawlstn3206@naver.com>
2023-12-04 12:45:05 +0900

    feat: fcm path prod, local 분리

--- src/main/java/jikgong/global/config/FCMConfig.java
+++ src/main/java/jikgong/global/config/FCMConfig.java
@@ -4,6 +4,7 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.FirebaseMessaging;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
@@ -14,9 +15,11 @@ import java.util.List;
 
 @Configuration
 public class FCMConfig {
+    @Value("${fcm.path}")
+    private String path;
     @Bean
     FirebaseMessaging firebaseMessaging() throws IOException {
-        ClassPathResource resource = new ClassPathResource("firebase/jikgong-firebase.json");
+        ClassPathResource resource = new ClassPathResource(path);
 
         InputStream refreshToken = resource.getInputStream();
 
