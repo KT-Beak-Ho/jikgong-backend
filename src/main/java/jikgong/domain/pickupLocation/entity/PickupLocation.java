9ff3a2fe40573b7b86c5932a6f576dd271fef128
dgjinsu <rlawlstn3206@naver.com>
2024-01-03 23:56:06 +0900

    refactor: 픽업 주소 enum -> String 으로 변경

--- src/main/java/jikgong/domain/pickupLocation/entity/PickupLocation.java
+++ src/main/java/jikgong/domain/pickupLocation/entity/PickupLocation.java
@@ -2,6 +2,7 @@ package jikgong.domain.pickupLocation.entity;
 
 import jakarta.persistence.*;
 import jikgong.domain.common.Address;
+import jikgong.domain.jobPost.entity.JobPost;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -15,11 +16,16 @@ public class PickupLocation {
     @Id @GeneratedValue
     @Column(name = "pickup_location_id")
     private Long id;
-    @Embedded
-    private Address address; // 픽업 도로명 주소
+
+    private String address; // 픽업 도로명 주소
+
+    @ManyToOne
+    @JoinColumn(name = "job_post_id")
+    private JobPost jobPost;
 
     @Builder
-    public PickupLocation(Address address) {
+    public PickupLocation(String address, JobPost jobPost) {
         this.address = address;
+        this.jobPost = jobPost;
     }
 }
