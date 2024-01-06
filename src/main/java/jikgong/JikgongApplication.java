6dcc3bc84a23f05b08ae11e5f00074632e39da7d
dgjinsu <rlawlstn3206@naver.com>
2024-01-06 17:01:07 +0900

    feat: 성능 시간 측정 AOP 적용

--- src/main/java/jikgong/JikgongApplication.java
+++ src/main/java/jikgong/JikgongApplication.java
@@ -3,11 +3,13 @@ package jikgong;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableFeignClients // feign client
 @EnableJpaAuditing // auditing
+@EnableAspectJAutoProxy // aop
 public class JikgongApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/jikgong/global/aop/TimingAdvisor.java
@@ -0,0 +1,34 @@
+package jikgong.global.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+@Slf4j
+public class TimingAdvisor {
+
+    @Pointcut("execution(public * jikgong.domain..*Service.*(..))")
+    public void pointcut() {}
+
+    @Around("pointcut()") // 어드바이스 + 포인트컷 설정
+    public void advice(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        String methodName = proceedingJoinPoint.getSignature().getName();
+
+        long start = System.currentTimeMillis();
+        proceedingJoinPoint.proceed(); // 타깃 메소드 호출
+
+        long end = System.currentTimeMillis();
+        long runningTime = end - start;
+
+        if (runningTime <= 1000) {
+            log.info("[정상 실행] method = {}, 실행시간 = {} ms", methodName, runningTime);
+        } else {
+            log.error("[!경고] [기준 실행 시간을 초과하였습니다] method = {}, 실행시간 = {} ms", methodName, runningTime);
+        }
+    }
+}
