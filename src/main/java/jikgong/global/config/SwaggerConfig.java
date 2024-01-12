59a9bc8ff751a3997a8d545fa2ce56053137c193
dgjinsu <rlawlstn3206@naver.com>
2024-01-12 19:51:15 +0900

    refactor: swagger Config 수정

--- src/main/java/jikgong/global/config/SwaggerConfig.java
+++ src/main/java/jikgong/global/config/SwaggerConfig.java
@@ -14,7 +14,7 @@ import java.util.Arrays;
 
 @OpenAPIDefinition(
         info = @Info(title = "jikgong API 명세서",
-                description = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJwaG9uZSI6IjAxMDEyMzQ1Njc4IiwiZXhwIjoxNzA5NjQwMzc5fQ.KmnnBZIP2uDI3GeJFoaMETmI2326S_kTLYr4-y4mfzs  \neyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJwaG9uZSI6IjAxMDExMTExMTExIiwiZXhwIjoxNzEwMjMwMzE1fQ.I8h6DA2cHHa7A3uFR55b7IyJkQ6Sc_hSqc-BEH1QsvE",
+                description = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJwaG9uZSI6IjAxMDEyMzQ1Njc4IiwiZXhwIjoxNzA5NjQwMzc5fQ.KmnnBZIP2uDI3GeJFoaMETmI2326S_kTLYr4-y4mfzs  \neyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJwaG9uZSI6IjAxMDExMTExMTExIiwiZXhwIjoxNzEwMjMwMzE1fQ.I8h6DA2cHHa7A3uFR55b7IyJkQ6Sc_hSqc-BEH1QsvE  \n eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJwaG9uZSI6IjAxMDIyMjIyMjIyIiwiZXhwIjoxNzEwMjM5NzcyfQ.ielaw53hKwae100UoXJ39zJ-8YXF5KqZqOMUJIWlgrA",
                 version = "v1"))
 @Configuration
 public class SwaggerConfig {
