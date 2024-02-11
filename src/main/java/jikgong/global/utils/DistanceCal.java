304ce5a912135025e5d95aead9b8d7c61f6baf0e
dgjinsu <rlawlstn3206@naver.com>
2024-02-11 14:00:46 +0900

    feat: 하버사인 알고리즘 오버로딩

--- src/main/java/jikgong/global/utils/DistanceCal.java
+++ src/main/java/jikgong/global/utils/DistanceCal.java
@@ -1,5 +1,6 @@
 package jikgong.global.utils;
 
+import jikgong.domain.common.Address;
 import jikgong.domain.jobPost.entity.JobPost;
 import jikgong.domain.jobPost.entity.QJobPost;
 import jikgong.domain.location.entity.Location;
@@ -15,6 +16,34 @@ public class DistanceCal {
         // 일자리 위치
         double lat2 = Math.toRadians(jobPost.getAddress().getLatitude());
         double lon2 = Math.toRadians(jobPost.getAddress().getLongitude());
+
+        // 위도 및 경도 간의 차이 계산
+        double dLat = lat2 - lat1;
+        double dLon = lon2 - lon1;
+
+        // Haversine 공식 계산
+        double a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+                Math.cos(lat1) * Math.cos(lat2) *
+                        Math.sin(dLon / 2) * Math.sin(dLon / 2);
+
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+        // 거리 계산
+        double distance = earthRadius * c;
+
+        return distance;
+    }
+
+    public static Double getDistance(Address projectAddress, Location location) {
+        double earthRadius = 6371; // 지구 반지름
+
+        // 노동자 위치
+        double lat1 = Math.toRadians(location.getAddress().getLatitude());
+        double lon1 = Math.toRadians(location.getAddress().getLongitude());
+
+        // 일자리 위치
+        double lat2 = Math.toRadians(projectAddress.getLatitude());
+        double lon2 = Math.toRadians(projectAddress.getLongitude());
 
         // 위도 및 경도 간의 차이 계산
         double dLat = lat2 - lat1;
