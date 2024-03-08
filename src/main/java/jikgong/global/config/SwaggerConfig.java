355b8c90db12f934ec91041d0264ab3626f0fff2
dgjinsu <rlawlstn3206@naver.com>
2024-03-08 16:00:40 +0900

    feat: api 테스트 시 사용할 토큰 업데이트

--- src/main/java/jikgong/global/config/SwaggerConfig.java
+++ src/main/java/jikgong/global/config/SwaggerConfig.java
@@ -14,10 +14,13 @@ import java.util.Arrays;
 
 @OpenAPIDefinition(
         info = @Info(title = "jikgong API 명세서",
-                description = "노동자 계정: 아래 3개   " +
-                        "\neyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJwaG9uZSI6IjAxMDEyMzQ1Njc4IiwiZXhwIjoxNzA5NjQwMzc5fQ.KmnnBZIP2uDI3GeJFoaMETmI2326S_kTLYr4-y4mfzs  " +
-                        "\neyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJwaG9uZSI6IjAxMDExMTExMTExIiwiZXhwIjoxNzEwMjMwMzE1fQ.I8h6DA2cHHa7A3uFR55b7IyJkQ6Sc_hSqc-BEH1QsvE  " +
-                        "\n eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJwaG9uZSI6IjAxMDIyMjIyMjIyIiwiZXhwIjoxNzEwMjM5NzcyfQ.ielaw53hKwae100UoXJ39zJ-8YXF5KqZqOMUJIWlgrA",
+                description = "기업 계정: 아래 1개   " +
+                        "\neyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJwaG9uZSI6IjAxMDEyMzQ1Njc4IiwiZXhwIjoxNzE1MDYzMDAyfQ.iCyhNWDzyP4pjGUaV37bV4jNY13QyFiigyBIn1I0jTs    " +
+                        "\n노동자 계정: 아래 4개    " +
+                        "\neyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJwaG9uZSI6IjAxMDExMTExMTExIiwiZXhwIjoxNzE1MDYyODAxfQ.mEyrVB4gvQb5fUgtB6Z8iFhYgdPyYENSmg8Y8FM8IXA    " +
+                        "\neyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJwaG9uZSI6IjAxMDIyMjIyMjIyIiwiZXhwIjoxNzE1MDYyODI3fQ.nwI6dub7Pv6e-k_jKiF62IjeeAVKavTnQRf8c2w9gAg    " +
+                        "\neyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJwaG9uZSI6IjAxMDMzMzMzMzMzIiwiZXhwIjoxNzE1MDYyODQwfQ.XlO3rkTM_loSw8QMvGGmr07N5UdUoLOEQ36QBthg1Q8    " +
+                        "\neyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJwaG9uZSI6IjAxMDQ0NDQ0NDQ0IiwiZXhwIjoxNzE1MDYyODcwfQ.-7Fe9BmZlx13GF_zgZOALJpwwXrpJpLhN76KcxLKmBA",
                 version = "v1"))
 @Configuration
 public class SwaggerConfig {
@@ -28,7 +31,7 @@ public class SwaggerConfig {
 
     // JWT + swagger
     @Bean
-    public OpenAPI openAPI(){
+    public OpenAPI openAPI() {
         SecurityScheme securityScheme = new SecurityScheme()
                 .type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
                 .in(SecurityScheme.In.HEADER).name("Authorization");
