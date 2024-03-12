308af5ff02c05a8872156891f61a41b9d5c9088a
dgjinsu <rlawlstn3206@naver.com>
2024-03-12 19:49:25 +0900

    feat: 지원 자동 취소 스케줄러 개발

---
+++ src/main/java/jikgong/global/batch/scheduler/ApplyCancelScheduler.java
@@ -0,0 +1,41 @@
+package jikgong.global.batch.scheduler;
+
+import jikgong.global.batch.job.ApplyJobConfig;
+import jikgong.global.exception.CustomException;
+import jikgong.global.exception.ErrorCode;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameter;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class ApplyCancelScheduler {
+    private final JobLauncher jobLauncher;
+    private final Job applyJobConfig;
+
+    @Scheduled(cron = "* 05 00 * * *") // 초, 분, 시, 일, 월, 주, 년(생략 가능)
+    @Scheduled(fixedDelay=10000) // 단위: ms
+    public void applyCancel() {
+        // job parameter 설정
+        Map<String, JobParameter<?>> confMap = new HashMap<>();
+        confMap.put("time", new JobParameter(System.currentTimeMillis(), Long.class));
+        JobParameters jobParameters = new JobParameters(confMap);
+        //JobParamter의 역할은 반복해서 실행되는 Job의 유일한 ID이다.
+        //Job Parameter에 동일한 값이 세팅되면 두번째부터 실행이 안되기 때문이다.
+
+        try {
+            jobLauncher.run(applyJobConfig, jobParameters);
+        } catch (Exception e) {
+            throw new CustomException(ErrorCode.SCHEDULER_INTERNAL_ERROR);
+        }
+    }
+}
