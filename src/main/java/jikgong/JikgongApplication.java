2a53aeb5ecfd7b6e819c05c48fe4e4b383c0da1c
dgjinsu <rlawlstn3206@naver.com>
2024-03-13 23:29:05 +0900

    feat: 스케줄링, batch 관련 설정 추가

--- src/main/java/jikgong/JikgongApplication.java
+++ src/main/java/jikgong/JikgongApplication.java
@@ -6,11 +6,13 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableFeignClients // feign client
 @EnableJpaAuditing // auditing
 @EnableAspectJAutoProxy // aop
+@EnableScheduling // scheduling
 @EnableAsync
 public class JikgongApplication {
 
