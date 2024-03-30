43d24d69a5579a974f0135d5c8e95ec8536b0846
dgjinsu <rlawlstn3206@naver.com>
2024-03-30 15:39:27 +0900

    rename: headhunting -> offer 디렉토리 명 수정

--- src/main/java/jikgong/domain/jobPost/dtos/offer/JobPostListResponse.java
+++ src/main/java/jikgong/domain/jobPost/dtos/offer/JobPostListResponse.java
@@ -1,4 +1,4 @@
-package jikgong.domain.jobPost.dtos.headhunting;
+package jikgong.domain.jobPost.dtos.offer;
 
 import jikgong.domain.jobPost.entity.JobPost;
 import jikgong.domain.jobPost.entity.Tech;
@@ -7,7 +7,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
-import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
 
@@ -18,17 +17,21 @@ public class JobPostListResponse {
     private Long jobPostId;
     private Tech tech; // 타입
     private String title; // 모집 공고 제목
-    private List<WorkDateResponse> availableDate; // 제안 가능 날짜
+    List<WorkDateResponse> availableDate; // 제안 가능 날짜 리스트
+    List<WorkDateResponse> fullCapacityDate; // 제안 불가능 날짜 (인원 마감)
+    List<WorkDateResponse> pastDate; // 제안 불가능 날짜 (날짜 지남)
     private LocalTime startTime; // 시작 시간
     private LocalTime endTime; // 종료 시간
     private Integer wage; // 임금
 
-    public static JobPostListResponse from(JobPost jobPost, List<WorkDateResponse> availableDate) {
+    public static JobPostListResponse from(JobPost jobPost, List<WorkDateResponse> availableDate, List<WorkDateResponse> unavailableDate, List<WorkDateResponse> pastDate) {
         return JobPostListResponse.builder()
                 .jobPostId(jobPost.getId())
                 .tech(jobPost.getTech())
                 .title(jobPost.getTitle())
                 .availableDate(availableDate)
+                .fullCapacityDate(unavailableDate)
+                .pastDate(pastDate)
                 .startTime(jobPost.getStartTime())
                 .endTime(jobPost.getEndTime())
                 .wage(jobPost.getWage())
--- src/main/java/jikgong/domain/offer/dtos/company/SelectOfferJobPostResponse.java
+++ src/main/java/jikgong/domain/offer/dtos/company/SelectOfferJobPostResponse.java
@@ -1,6 +1,6 @@
 package jikgong.domain.offer.dtos.company;
 
-import jikgong.domain.jobPost.dtos.headhunting.JobPostListResponse;
+import jikgong.domain.jobPost.dtos.offer.JobPostListResponse;
 import jikgong.domain.jobPost.entity.JobPost;
 import jikgong.domain.member.entity.Member;
 import jikgong.domain.workDate.dtos.WorkDateResponse;
@@ -10,9 +10,9 @@ import lombok.Builder;
 import lombok.Getter;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 
 @AllArgsConstructor
 @Getter
@@ -21,21 +21,36 @@ public class SelectOfferJobPostResponse {
     private String workerName;
     private List<JobPostListResponse> jobPostListResponseList;
 
-    public static SelectOfferJobPostResponse from(List<JobPost> jobPostList, Member member, Set<LocalDate> cantWorkDateSet) {
+    public static SelectOfferJobPostResponse from(List<JobPost> jobPostList, Member member) {
         List<JobPostListResponse> jobPostListResponseList = new ArrayList<>();
         LocalDate now = LocalDate.now();
 
+        // 여러 공고 한번에 제안 가능
+        // 공고 하나당 제안 가능한 날짜 추출
         for (JobPost jobPost : jobPostList) {
-            List<WorkDateResponse> availableDate = new ArrayList<>();
+            List<WorkDateResponse> availableDate = new ArrayList<>(); // 제안 가능 날짜 리스트
+            List<WorkDateResponse> fullCapacityDate = new ArrayList<>(); // 제안 불가능 날짜 (인원 마감)
+            List<WorkDateResponse> pastDate = new ArrayList<>(); // 제안 불가능 날짜 (날짜 지남)
+
+            // workDate 조회
             List<WorkDate> jobPostWorkDateList = jobPost.getWorkDateList();
-            for (WorkDate jobPostWorkDate : jobPostWorkDateList) {
-                // 노동자가 이미 일하는 날짜를 제외
-                // 당일, 과거 날짜 제외
-                if (!cantWorkDateSet.contains(jobPostWorkDate.getDate()) && jobPostWorkDate.getDate().isAfter(now)) {
-                    availableDate.add(WorkDateResponse.from(jobPostWorkDate));
+            for (WorkDate workDate : jobPostWorkDateList) {
+                // 출역일 전 인지 체크
+                if (now.isAfter(workDate.getDate())) {
+                    pastDate.add(WorkDateResponse.from(workDate));
                 }
+                if (now.isEqual(workDate.getDate()) && LocalTime.now().isAfter(jobPost.getStartTime())) {
+                    pastDate.add(WorkDateResponse.from(workDate));
+                }
+
+                // 모집된 인원이 전부 찼는지 체크
+                if (workDate.getRegisteredNum() >= workDate.getRegisteredNum()) {
+                    fullCapacityDate.add(WorkDateResponse.from(workDate));
+                }
+
+                availableDate.add(WorkDateResponse.from(workDate));
             }
-            jobPostListResponseList.add(JobPostListResponse.from(jobPost, availableDate));
+            jobPostListResponseList.add(JobPostListResponse.from(jobPost, availableDate, fullCapacityDate, pastDate));
         }
 
         return SelectOfferJobPostResponse.builder()
