1a2453fee341e79eed661749f30c7323f6a701e9
dgjinsu <rlawlstn3206@naver.com>
2024-04-20 16:16:33 +0900

    feat: CusTomException 생성자 오버로딩

--- src/main/java/jikgong/global/exception/CustomException.java
+++ src/main/java/jikgong/global/exception/CustomException.java
@@ -1,14 +1,22 @@
 package jikgong.global.exception;
 
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
 @Getter
 public class CustomException extends RuntimeException {
-    private ErrorCode errorCode;
+    private final HttpStatus status;
+    private final String errorMessage;
 
     // ErrorCode 생성자
     public CustomException(ErrorCode errorCode) {
         super(errorCode.getErrorMessage());
-        this.errorCode = errorCode;
+        this.status = errorCode.getStatus();
+        this.errorMessage = errorCode.getErrorMessage();
+    }
+    public CustomException(HttpStatus status, String errorMessage) {
+        super(errorMessage);
+        this.status = status;
+        this.errorMessage = errorMessage;
     }
 }
