f4ced8bc4f185e1c7fa1065f6fa1bb689c9a6707
dgjinsu <rlawlstn3206@naver.com>
2024-05-03 20:06:30 +0900

    feat: SkipPolicy 개발

---
+++ src/main/java/jikgong/global/batch/job/CustomSkipPolicy.java
@@ -0,0 +1,27 @@
+package jikgong.global.batch.job;
+
+import jikgong.global.exception.CustomException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.step.skip.SkipPolicy;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+@Slf4j
+public class CustomSkipPolicy implements SkipPolicy {
+    private int skipCount = 0;
+
+    @Override
+    public boolean shouldSkip(Throwable t, long skipCount) {
+        if (skipCount > 5) {
+            log.error("skip한 item 5개 초과");
+            return false; // 최대 Skip 횟수 초과 시 Skip 중지
+        }
+
+        log.warn("item 처리 중 예외 발생: {}", t.getMessage());
+        this.skipCount++;
+        return true;
+    }
+}
