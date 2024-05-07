846ea4f53f7348569fba6f8f0e4f23caac3a518a
dgjinsu <rlawlstn3206@naver.com>
2024-05-07 16:16:06 +0900

    feat: 노동자가 scrap한 jobPostId Set 반환 쿼리 추가

--- src/main/java/jikgong/domain/scrap/repository/ScrapRepository.java
+++ src/main/java/jikgong/domain/scrap/repository/ScrapRepository.java
@@ -8,9 +8,13 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 @Repository
 public interface ScrapRepository extends JpaRepository<Scrap, Long> {
     @Query("select s from Scrap s where s.member.id = :memberId and s.jobPost.id = :jobPostId")
     Optional<Scrap> findByMemberAndJobPost(@Param("memberId") Long memberId, @Param("jobPostId") Long jobPostId);
+
+    @Query("select s.jobPost.id from Scrap s where s.member.id = :memberId")
+    Set<Long> findScrapJobPostIdByMember(@Param("memberId") Long memberId);
 }
