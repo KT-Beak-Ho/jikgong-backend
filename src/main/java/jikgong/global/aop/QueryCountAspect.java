846ba48c354517e1e65e77f88a3bf57fe222b57e
dgjinsu <rlawlstn3206@naver.com>
2024-05-21 15:32:33 +0900

    feat: 쿼리 카운팅 기능 AOP 등록

---
+++ src/main/java/jikgong/global/aop/QueryCountAspect.java
@@ -0,0 +1,32 @@
+package jikgong.global.aop;
+
+import jikgong.global.queryCount.ConnectionHandler;
+import jikgong.global.queryCount.QueryCounter;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Proxy;
+
+@Aspect
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class QueryCountAspect {
+
+    private final QueryCounter queryCounter;
+
+    @Around("execution(* javax.sql.DataSource.getConnection(..))")
+    public Object getConnection(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        Object connection = proceedingJoinPoint.proceed();
+        return Proxy.newProxyInstance(
+                connection.getClass().getClassLoader(),
+                connection.getClass().getInterfaces(),
+                new ConnectionHandler(connection, queryCounter)
+        );
+    }
+}
