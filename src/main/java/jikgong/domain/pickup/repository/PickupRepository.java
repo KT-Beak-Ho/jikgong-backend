670692c643d4a7741726e88f28f0205a0ed9f33f
dgjinsu <rlawlstn3206@naver.com>
2024-08-22 20:09:05 +0900

    refactor: cascade -> 쿼리 날려 삭제로 변경

--- src/main/java/jikgong/domain/pickup/repository/PickupRepository.java
+++ src/main/java/jikgong/domain/pickup/repository/PickupRepository.java
@@ -2,9 +2,15 @@ package jikgong.domain.pickup.repository;
 
 import jikgong.domain.pickup.entity.Pickup;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PickupRepository extends JpaRepository<Pickup, Long> {
 
+    @Modifying
+    @Query("delete from Pickup p where p.jobPost.id = :jobPostId")
+    void deleteByJobPost(@Param("jobPostId") Long jobPostId);
 }
