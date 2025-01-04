02136bdf51f2ac698d3ab0fbc08f4263640eb184
dgjinsu <rlawlstn3206@naver.com>
2025-01-04 11:32:18 +0900

    chore: domain 변경으로 인한 swagger 설정 파일 변경

--- src/main/java/jikgong/global/config/SwaggerConfig.java
+++ src/main/java/jikgong/global/config/SwaggerConfig.java
@@ -12,24 +12,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @OpenAPIDefinition(
-    info = @Info(title = "jikgong API 명세서",
-        description = "기업 계정: 아래 1개   " +
-            "\neyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJsb2dpbklkIjoiYWJjZGVmZzAiLCJleHAiOjE3Mzc3MDMyMTZ9.c-UN8qz7EzBrIDk7Mj03ohW7yXcDxcD6xtjyNV4__9Y    "
-            +
-            "\n노동자 계정: 아래 4개    " +
-            "\neyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJsb2dpbklkIjoiYWJjZGVmZzEiLCJleHAiOjE3Mzc3MDMyNDl9.QQcmJqutM61IqFTe-a4BPL8QCrnHdQGx9kOcoLw9yEQ    "
-            +
-            "\neyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJsb2dpbklkIjoiYWJjZGVmZzIiLCJleHAiOjE3Mzc3MDMyNjN9.PzT8hs8T9K6EpEZ1C4fD_rU_2Q91ezdJOUEJX8xqv9A    "
-            +
-            "\neyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJsb2dpbklkIjoiYWJjZGVmZzMiLCJleHAiOjE3Mzc3MDMyNzh9.nUuAmr330GYeyHO6UUffBQpIxn8civq6cD6C6kKIB1E    "
-            +
-            "\neyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJsb2dpbklkIjoiYWJjZGVmZzQiLCJleHAiOjE3MjYyMjY4MjB9.pCs2S7lzylPgIxD0t0IQ7bO89GaRZi1QoFcvjIc2W2c",
-        version = "v1"),
+    info = @Info(title = "jikgong API 명세서", version = "v1"),
     servers = {
-        @Server(url = "https://asdfdsas.p-e.kr", description = "Default Server"),
+        @Server(url = "https://jikgong.p-e.kr", description = "Default Server"),
         @Server(url = "http://localhost:8080", description = "Local Server"),
-        @Server(url = "http://14.42.195.195:8080", description = "Home Server")
-    })
+    }
+)
 @Configuration
 public class SwaggerConfig {
 
